69281a0a3cbdf3c42186fbf0f2628a99851596c4
yijingping <jingping.yi@gmail.com>
2016-12-15 18:02:33 +0800

    fix 迁移sql脚本

--- data/update.sql
+++ data/update.sql
@@ -30,11 +30,11 @@ SELECT * FROM data_fupin.yunnan_all_pinkunhu_2014 where county='镇雄县' or co
 
 CREATE TABLE poormining.yunnan_all_pinkunhu_2015
 AS
-SELECT * FROM data_fupin.yunnan_all_pinkunhu_2015 where county='镇雄县' or county ='彝良县'
+SELECT * FROM data_fupin.yunnan_all_pinkunhu_2015 where county='镇雄县' or county ='彝良县';
 
 CREATE TABLE poormining.yunnan_all_pinkunhu_2016
 AS
-SELECT * FROM data_fupin.yunnan_all_pinkunhu_2016 where county='镇雄县' or county ='彝良县'
+SELECT * FROM data_fupin.yunnan_all_pinkunhu_2016 where county='镇雄县' or county ='彝良县';
 
 -- 去除秘密字段
 update `yunnan_all_pinkunhu_2014` set `name` = concat(left(`name`, 1), '**'), `bank_name`= concat(left(`bank_number`, 0), '******'), `bank_number` = concat(left(`bank_number`, 0), '******'),`bank_name`= concat(left(`bank_number`, 0), '******'), `bank_number` = concat(left(`bank_number`, 0), '******'),`call_number` = concat(left(`call_number`, 3), '********'),`card_number` = '', `province`='', `city`='', `town`='', `village`='', `group`='';
